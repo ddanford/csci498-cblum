1021898a420e894a53e40b530f86de2eb86820f2
Carl Blum <cblum@mymail.mines.edu>
2011-02-26 17:49:45 -0700

    Assembler with symbols created, tested, and finished. Project06 complete

--- project06/assembler.py
+++ project06/assembler.py
@@ -1,13 +1,22 @@
 import sys
 
 # Opens the file and readys the .hack file for writing
+# Begins translation of asm file and puts binary output in the .hack file
 def initializer ( filename ):
 	if (filepath.partition('.asm')[1] != '.asm'):
 		print('Invalid file. Must be a .asm file')
 		quit()
 	
-	asmfile = open(filepath, 'r')
-	hackfile = open(filepath.partition('.asm')[0] + '.hack', 'w')
+	try:
+		asmfile = open(filepath, 'r')
+	except:
+		print("Could not open input file: " + filepath)
+		quit()
+	try:
+		hackfile = open(filepath.partition('.asm')[0] + '.mine.hack', 'w')
+	except:
+		print("Could not open output file: " + filepath.partition('.asm')[0] + '.hack.mine')
+		quit()
 	
 	asmcommands = asmfile.readlines()
 	commandcounter = 0
@@ -18,13 +27,21 @@ def initializer ( filename ):
 	while ( hasMoreCommands ( commandcounter, numberofcommands ) ):
 		commandtype = commandType (asmcommands[commandcounter])
 		if (commandtype == 'A_COMMAND'):
+			linecounter = advance( linecounter )
+		if (commandtype == 'C_COMMAND'):
+			linecounter = advance( linecounter )
+		if (commandtype == 'L_COMMAND'):
+			addEntry(asmcommands[commandcounter].partition('(')[2].partition(')')[0], linecounter)
+		commandcounter = advance( commandcounter )
+		
+	commandcounter = 0
+	
+	while ( hasMoreCommands ( commandcounter, numberofcommands ) ):
+		commandtype = commandType (asmcommands[commandcounter])
+		if (commandtype == 'A_COMMAND'):
 			hackfile.write('0' + symbol(asmcommands[commandcounter]) + '\n')
 		if (commandtype == 'C_COMMAND'):
 			hackfile.write('111' + comp(asmcommands[commandcounter]) + dest(asmcommands[commandcounter]) + jump(asmcommands[commandcounter]) + '\n')
-		if (commandtype == 'L_COMMAND'):
-			print('L')
-		if (commandtype == 'OTHER'):
-			print('other')
 		commandcounter = advance( commandcounter )
 	
 	asmfile.close()
@@ -42,8 +59,8 @@ def advance ( commandcounter ):
 	
 # Returns the command type of the next command: A_COMMAND, C_COMMAND, or L_COMMAND
 def commandType ( codeline ):
-	if (codeline.partition('//')[1] == '//'):
-		return 'OTHER'
+	if (codeline.partition('//')[1] == '//' and codeline.partition('//')[0] == ''):
+		return 'COMMENT'
 	if (codeline.partition('@')[1] == '@'):
 		return 'A_COMMAND'
 	if (codeline.partition('=')[1] == '=' or codeline.partition(';')[1] == ';'):
@@ -58,7 +75,13 @@ def symbol ( code ):
 	try:
 		return bin(int(codesymbol))[2:].zfill(15)
 	except:
-		return '000000000000000'
+		if ( contains (codesymbol) ):
+			return bin(GetAddress(codesymbol))[2:].zfill(15)
+		else:
+			global symbolcounter
+			addEntry(codesymbol, symbolcounter)
+			symbolcounter = advance ( symbolcounter )
+			return bin(GetAddress(codesymbol))[2:].zfill(15)
 
 destmap = {
 'M': '001',
@@ -113,19 +136,19 @@ jumpmap = {
 # Returns the 3 bit string of binary digits of the dest mnemonic based on the assembly code
 def dest ( code ):
 	if (code.partition('=')[1] == '='):
-		return destmap[code.partition('=')[0]]
+		return destmap[code.partition('=')[0].replace('\t', '').strip()]
 	return '000'
 
 # Returns the 7 bit string of binary digits of the comp mnemonic based on the assembly code
 def comp ( code ):
-	compstring = code.partition('=')[2].partition('\n')[0]
+	compstring = code.partition('//')[0].partition('=')[2].partition('\n')[0].strip()
 	if ( compstring in compmap ):
 		return compmap[compstring]
-	return compmap[code.partition(';')[0]]
+	return compmap[code.partition(';')[0].replace('\t', '').strip()]
 
 # Returns the 3 bit string of binary digits of the jump mnemonic based on the assembly code
 def jump ( code ):
-	jumpstring = code.partition(';')[2].partition('\n')[0]
+	jumpstring = code.partition('//')[0].partition(';')[2].partition('\n')[0].strip()
 	if ( jumpstring in jumpmap ):
 		return jumpmap[jumpstring]
 	return '000'
@@ -133,34 +156,52 @@ def jump ( code ):
 
 # SymbolTable Module
 
-# Constructor, initializes the symbol table and gets it ready for additional entries
-def symbolContructor ():
-	quit()
-
+symboltable = {
+'SP': 0,
+'LCL': 1,
+'ARG': 2,
+'THIS': 3,
+'THAT': 4,
+'R0': 0,
+'R1': 1,
+'R2': 2,
+'R3': 3,
+'R4': 4,
+'R5': 5,
+'R6': 6,
+'R7': 7,
+'R8': 8,
+'R9': 9,
+'R10': 10,
+'R11': 11,
+'R12': 12,
+'R13': 13,
+'R14': 14,
+'R15': 15,
+'SCREEN': 16384,
+'KBD': 24
+}
 # adds a pair of a symbol and its value to the symbol table
 def addEntry ( symbol, value ):
-	quit()
+	symboltable[symbol] = value
 
 # Checks to see if the symbol is in the symbol table
 def contains ( symbol ):
-	quit()
+	if ( symbol in symboltable ):
+		return 1
+	return 0
 
 # Returns the value of the supplied symbol from the symbol table
 def GetAddress ( symbol ):
-	quit()
+	return symboltable[symbol]
 
 
-	
+# MAIN - begins the assembler
 if len(sys.argv) != 2:
 	print('Invalid syntax for assembler.py. Proper syntax is assembler.py [filepath]')
 	quit()
 
 filepath = sys.argv[1]
 
+symbolcounter = 16
 initializer (filepath)
-
-
-
-
-
-
