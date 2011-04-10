2e8bf65a2f1b06b8d9bc820cf57c151a16b4b310
Carl Blum <cblum@mymail.mines.edu>
2011-04-10 13:11:39 -0600

    Create JackTokenizer.py. Added functionality to walk through a directory and generate .xml files for each of the .jack files

---
+++ project10/JackTokenizer.py
@@ -0,0 +1,54 @@
+import sys
+import os
+
+def initializer ( filepath ):
+	#Checks to see if the path given is a directory. If it is, it iterates through each file in the directory to see if it is a .jack file
+	if (os.path.isdir(filepath)):
+		for subdirs, dirs, files in os.walk(filepath):
+			for inpath in files:
+				if filepath.endswith("\\"):
+					filepath = filepath.replace("\\", "/")
+				elif not(filepath.endswith("/")):
+					filepath = filepath+"/"
+				if (inpath.partition('.jack')[1] == '.jack'):
+					try:
+						jackfile = open(filepath+inpath, 'r')
+					except:
+						print("Could not open input file: " + filepath)
+						quit()
+					try:
+						xmlfile = open((filepath+inpath).partition('.jack')[0] + 'T.generated.xml', 'w')
+					except:
+						print("Could not open output file: " + filepath.partition('.jack')[0] + 'T.generated.xml')
+						quit()
+					tokenizeFile ( jackfile, xmlfile )
+					jackfile.close()
+					xmlfile.close()
+	else:
+		if (filepath.partition('.vm')[1] != '.jack'):
+			print('Invalid file. Must be a .jack file')
+			quit()
+		try:
+			jackfile = open(filepath, 'r')
+		except:
+			print("Could not open input file: " + filepath)
+			quit()
+		try:
+			xmlfile = open(filepath.partition('.jack')[0] + 'T.generated.xml', 'w')
+		except:
+			print("Could not open output file: " + filepath.partition('.jack')[0] + 'T.generated.xml')
+			quit()
+		tokenizeFile ( jackfile, xmlfile )
+		vmfile.close()
+		asmfile.close()
+		
+def tokenizeFile ( jackfile, xmlfile ):
+	return 0;
+
+
+if len(sys.argv) != 2:
+	print('Invalid syntax for JackTokenizer.py. Proper syntax is JackTokenizer.py [filepath | directory]')
+	quit()
+	
+filepath = sys.argv[1]
+initializer (filepath)
